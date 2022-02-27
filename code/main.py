db5707a0fc1a20af94900c49fffe7a6eb1364b51
Max <kolxy@qq.com>
2022-02-26 23:14:43 -0800

    main

--- code/main.py
+++ code/main.py
@@ -1,6 +1,9 @@
-from networks.autoencoders import fakeAutoEncoder as fake
-def main():
-    print(fake.fakefunc())
+import sys
+
+def main(argv):
+    arguments = argv[1:]
+    for arg in arguments:
+        print(arg)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
--- code/networks/autoencoders/fakeAutoEncoder.py
+++
@@ -1,2 +0,0 @@
-def fakefunc():
-    return "FAKE FUNCTION"
