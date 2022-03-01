885c0c0bfca5948e0af7233e754c55563cc0e780
unknown <jarren.briscoe@gmail.com>
2022-03-01 15:29:22 -0800

    preprocessing refactor

--- code/utils/filePaths.py
+++ code/utils/filePaths.py
@@ -21,30 +21,34 @@ class binary_casas:
     _trainSuffix = "Train" + _fileExtension
     _testSuffix = "Test" + _fileExtension
 
-    _filePath = folder.data + "binaryCasas/" + folder.data + "{filename}"
-    _trainFile = "{filename}" + _trainSuffix
-    _testFile = "{filename}" + _testSuffix
+    _filePath = folder.data + "binaryCasas/" + folder.processed
+    _trainFile = _filePath + "{filename}" + _trainSuffix
+    _testFile = _filePath + "{filename}" + _testSuffix
 
     _homeName = "b{}"
     _home1Name = _homeName.format("1")
     _home2Name = _homeName.format("2")
     _home3Name = _homeName.format("3")
 
-    @staticmethod
-    def _create_home_data(homeName) -> common.ml_data:
-        return common.ml_data(
-            train=binary_casas._trainFile.format(filename=homeName),
-            test=binary_casas._testFile.format(filename=homeName)
-        )
 
+    h1Home = common.ml_data(
+        train=_trainFile.format(filename=_home1Name),
+        test = _testFile.format(filename=_home1Name)
+    )
 
+    h2Home = common.ml_data(
+        train=_trainFile.format(filename=_home2Name),
+        test = _testFile.format(filename=_home2Name)
+    )
+
+    h3Home = common.ml_data(
+        train=_trainFile.format(filename=_home3Name),
+        test = _testFile.format(filename=_home3Name)
+    )
+
+    allHomes = [h1Home, h2Home, h3Home]
 
     # h1Home = _create_home_data(_home1Name)
-    # h2Home = _create_home_data(_home2Name)
-    # h3Home = _create_home_data(_home3Name)
-    #
-    # allHomes = [h1Home, h2Home, h3Home]
-
     @staticmethod
     def _get_home(fileNames:common.ml_data):
         return fileNames.transform(pd.read_csv)
@@ -65,12 +69,6 @@ class binary_casas:
     def get_all_homes():
         return [binary_casas.get_home1(), binary_casas.get_home2(), binary_casas.get_home3()]
 
-# with binary_casas as bc:
-    # bc.h1Home = bc._create_home_data(bc._home1Name)
-    # bc.h2Home = bc._create_home_data(bc._home2Name)
-    # bc.h3Home = bc._create_home_data(bc._home3Name)
-    #
-    # allHomes = [bc.h1Home, bc.h2Home, bc.h3Home]
 
 
 @dataclass(frozen=True)
