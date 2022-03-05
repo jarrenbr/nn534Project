e455dbe0b0bdcd3c60ddd037acfe4ba1725a76d0
jbroot <jarren.briscoe@gmail.com>
2022-03-05 15:50:27 -0800

    default message

--- src/networks/commonBlocks.py
+++ src/networks/commonBlocks.py
@@ -4,23 +4,14 @@ from tensorflow.keras import layers as l
 
 from networks import defaults
 
-def conv_block(
+
+def block(
     x,
-    filters,
     activation,
-    kernel_size=3,
-    strides=2,
-    padding="same",
-    use_bias=True,
     use_bn=False,
     use_dropout=False,
-    drop_value=defaults.DROPOUT_PORTION,
+    drop_value = defaults.DROPOUT_PORTION
 ):
-    if use_bn:
-        use_bias = False
-    x = l.Conv1D(
-        filters, kernel_size, strides=strides, padding=padding, use_bias=use_bias
-    )(x)
     if use_bn:
         x = l.BatchNormalization()(x)
     x = activation(x)
@@ -28,18 +19,3 @@ def conv_block(
         x = l.Dropout(drop_value)(x)
     return x
 
-def trans_conv_block(
-    x,
-    filters,
-    activation,
-    kernel_size=3,
-    strides=2,
-    padding="same",
-    use_bias=True,
-    use_bn=False,
-    use_dropout=False,
-    drop_value=defaults.DROPOUT_PORTION,
-):
-
-
-    return x
--- src/networks/gans/stateFulGan.py
+++ src/networks/gans/stateFulGan.py
@@ -4,8 +4,9 @@ from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 
 import files.filePaths as fp
-from networks.gans import genApi
-from networks.gans import wgan
+from networks.gans import genApi, wgan
+from names import binaryCasasNames as bcNames
+from networks import commonBlocks as cBlocks, defaults
 
 MODEL_DIR = fp.folder.kmModel + "statefulGan/"
 # def classifier_naming
@@ -15,7 +16,16 @@ CRITIC_FILE = MODEL_DIR + "critic" + fp.extensions.kerasModel
 
 GENERATOR_BATCH_SIZE = 16
 CRITIC_BATCH_SIZE = 128
+N_TIME_STEPS = 32
+NOISE_DIM = 128
 
+def get_generator() -> keras.models.Model:
+    #goal: 32 X 48
+    inputLayer = keras.Input(
+        shape=(1, NOISE_DIM)
+    )
+    x = layers.Conv1DTranspose(filters=96, kernel_size=2, use_bias=False)(inputLayer)
+    x = cBlocks.block(x, activation=defaults.leaky_relu(), use_bn=True,)
 
 
 if __name__ == "__main__":
