d38c9ffd65029f93d6d1d589e285fe85d7b06e5b
jbroot <jarren.briscoe@gmail.com>
2022-03-05 16:12:57 -0800

    default message

--- src/networks/commonBlocks.py
+++ src/networks/commonBlocks.py
@@ -4,6 +4,12 @@ from tensorflow.keras import layers as l
 
 from networks import defaults
 
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers as l
+
+from networks import defaults
+
 class conv_args:
     def __init__(self, nFilters, kernelSize):
         self.nFilters = nFilters
@@ -24,3 +30,42 @@ def block(
         x = l.Dropout(drop_value)(x)
     return x
 
+
+
+def conv_block(
+    x,
+    filters,
+    activation,
+    kernel_size=3,
+    strides=2,
+    padding="same",
+    use_bias=True,
+    use_bn=False,
+    use_dropout=False,
+    drop_value=defaults.DROPOUT_PORTION,
+):
+    if use_bn:
+        use_bias = False
+    x = l.Conv1D(filters, kernel_size, strides=strides, padding=padding, use_bias=use_bias)(x)
+    x = block(
+        x,
+        activation=activation,
+        use_bn=use_bn, use_dropout=use_dropout, drop_value=drop_value,
+    )
+    return x
+
+def trans_conv_block(
+    x,
+    filters,
+    activation,
+    kernel_size=3,
+    strides=2,
+    padding="same",
+    use_bias=True,
+    use_bn=False,
+    use_dropout=False,
+    drop_value=defaults.DROPOUT_PORTION,
+):
+
+
+    return x
