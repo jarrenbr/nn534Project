b9694bd142b7b54e7f529838cf4d008452382cb2
Max <kolxy@qq.com>
2022-05-02 20:56:43 -0700

    classifier

--- src/networks/classifiers/binaryCasasClassifier.py
+++ src/networks/classifiers/binaryCasasClassifier.py
@@ -6,78 +6,106 @@ from pathlib import Path
 import pandas as pd
 import glob
 import numpy as np
+import os
 
 from processData.binaryCasasProcess import binaryCasasData as bcData
 from names import binaryCasasNames as bcNames
 from networks import commonBlocks as cBlocks, defaults
 from utils import common, globalVars as gv
 from processData import windowsGenerator as wg
+from networks.gans import stateFulGan as sfg
+from networks.gans import genApi, wgan
+import matplotlib.pyplot as plt
+from processData.binaryCasasProcess import postProcess as pp
 
 BATCH_SIZE = 64
-N_TIME_STEPS = 32
-EPOCHS = 2 if gv.DEBUG else 6
+N_TIME_STEPS = 16
+EPOCHS = 20
+PATH_ASSETS = os.getcwd()
 
 def run():
-    df = getSynthData()
-    trainModel(df)
-
-"""
-Merge multiple .csv files into one dataframe
-"""
-def getSynthData():
-    synth_dir = Path(__file__).parent.parent.parent/'synthetic-data'
-    all_files = glob.glob(str(synth_dir) + "/*.csv")
-    print(all_files)
-    df_from_each_file = (pd.read_csv(f, header = None) for f in all_files)
-    df = pd.concat(df_from_each_file, ignore_index=True)
-    return df
-
-def trainModel(df: pd.DataFrame):
-    train=df.sample(frac=0.8, random_state=200) #random state is a seed value
-    test=df.drop(train.index)
-    train_gen = getGen(train)
-    test_gen = getGen(test)
-    print(df.shape)
-
-def getGen(df: pd.DataFrame) -> wg.windows_generator:
-    return bcData.df_to_gen(df, wg.x_y_split_windows, batchSize = BATCH_SIZE, nTimesteps = N_TIME_STEPS, xyPivot = bcNames.pivots.activities.start, stride = None)
+    # trtr("CNN_on_real")
 
 
+    # loadGan = True
+    loadGan = False
+    gan = sfg.get_gan(loadGan)
+    gan = sfg.run_gan(gan, 20)
+
+    genOut = []
+    for sampleNum in range(5000):
+        genOut.append(genApi.get_gen_out(gan.generator, sfg.NOISE_DIM, batchSize=sfg.BATCH_SIZE))
+
+    #np.ndarray in shape (samples, time steps, features)
+    genOut = np.concatenate(genOut, axis=0)
+    genOut = pp.gen_out_to_real_normalized(genOut)
+
+    x,y = genOut[...,:bcNames.nFeatures], genOut[...,-1,bcNames.nFeatures:]
+    print(x.shape, y.shape)
+    tstr(x, y, "stateful_GAN")
+
+def tstr(x, y, title): 
+    allHomes = bcData.get_all_homes_as_xy_split_gen(
+        batchSize=BATCH_SIZE, nTimesteps=N_TIME_STEPS,
+        xyPivot=bcNames.pivots.activities.start, firstN=gv.DATA_AMT
+    )
+    model = basic_cnn()
+    history = model.fit(x, y, epochs=EPOCHS, steps_per_epoch = defaults.STEPS_PER_EPOCH,
+                        validation_data=allHomes[0].data.test.gen, validation_steps=defaults.VALIDATION_STEPS)
+    test_result_0 = model.evaluate(allHomes[0].data.test.gen, steps=defaults.VALIDATION_STEPS, verbose=2)
+    test_result_1 = model.evaluate(allHomes[1].data.test.gen, steps=defaults.VALIDATION_STEPS, verbose=2)
+    test_result_2 = model.evaluate(allHomes[2].data.test.gen, steps=defaults.VALIDATION_STEPS, verbose=2)
+    print(history.history)
+    plt.figure()
+    plt.title(title)
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0, 1])
+    plt.legend(loc='lower right')
+    plt.savefig(PATH_ASSETS + "/img/accuracy/" + title + ".png")
+    print(test_result_0[1])
+    print(test_result_1[1])
+    print(test_result_2[1])
+
+def trtr(title):
+    allHomes = bcData.get_all_homes_as_xy_split_gen(
+        batchSize=BATCH_SIZE, nTimesteps=N_TIME_STEPS,
+        xyPivot=bcNames.pivots.activities.start, firstN=gv.DATA_AMT
+    )
+    model = basic_cnn()
+    history = model.fit(allHomes[0].data.train.gen, epochs=EPOCHS, steps_per_epoch = defaults.STEPS_PER_EPOCH,
+                        validation_data=allHomes[0].data.test.gen, validation_steps=defaults.VALIDATION_STEPS)
+    test_result_0 = model.evaluate(allHomes[0].data.test.gen, steps=defaults.VALIDATION_STEPS, verbose=2)
+    test_result_1 = model.evaluate(allHomes[1].data.test.gen, steps=defaults.VALIDATION_STEPS, verbose=2)
+    test_result_2 = model.evaluate(allHomes[2].data.test.gen, steps=defaults.VALIDATION_STEPS, verbose=2)
+    print(history.history)
+    plt.figure()
+    plt.title(title)
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0, 1])
+    plt.legend(loc='lower right')
+    plt.savefig(PATH_ASSETS + "/img/accuracy/" + title + ".png")
+    print(test_result_0[1])
+    print(test_result_1[1])
+    print(test_result_2[1])
 
 def basic_cnn() -> keras.models.Model:
-    inputLayer = keras.Input(shape=(N_TIME_STEPS, len(bcNames.features))) #should be 16 X 48
-    x = cBlocks.conv_block(inputLayer, 20, defaults.leaky_relu(),) #8 timesteps
-    x = cBlocks.conv_block(x, 12, defaults.leaky_relu()) #4 timesteps
+    inputLayer = keras.Input(shape=(N_TIME_STEPS, len(bcNames.features))) #should be 48 channels
+    x = cBlocks.conv_block(inputLayer, 24, defaults.leaky_relu(),) #16 timesteps
+    x = cBlocks.conv_block(x, 15, defaults.leaky_relu()) #8 timesteps
+    x = cBlocks.conv_block(x, 11, defaults.leaky_relu()) #4 timesteps
     x = cBlocks.conv_block(x, 8, defaults.leaky_relu()) #2 timesteps
     x = cBlocks.conv_block(x, len(bcNames.allActivities), activation=keras.activations.softmax)
     x = l.Flatten()(x)
     model = keras.models.Model(inputLayer, x, name="Basic_CNN_Classifier")
     model.compile(loss = keras.losses.CategoricalCrossentropy(),
-                  optimizer = defaults.optimizer(), metrics = defaults.METRICS)
+                  optimizer = defaults.optimizer(), metrics = ['accuracy'])
     return model
 
-#Nathan uses temporal CNNs
-
-def multi_head_cnn():
-    pass
-
-def run_classifiers(data:common.ml_data):
-    model = basic_cnn()
-    # print(model.summary())
-    history = model.fit(data.train.gen, epochs=EPOCHS, steps_per_epoch = defaults.STEPS_PER_EPOCH,
-                        validation_data=data.test.gen, validation_steps=defaults.VALIDATION_STEPS)
-    preds = model.predict(data.test.gen, steps=defaults.PREDICT_STEPS)
-    predsOneHot = np.zeros(preds.shape)
-    predsOneHot[
-        preds.argmax(axis=1).reshape((-1,1))
-    ] = 1
-    return model, history
-
 if __name__ == "__main__":
-    allHomes = bcData.get_all_homes_as_xy_split_gen(
-        batchSize=BATCH_SIZE, nTimesteps=N_TIME_STEPS,
-        xyPivot=bcNames.pivots.activities.start, firstN=gv.DATA_AMT
-    )
-    run_classifiers(allHomes[0].data)
-    # run()
-    exit()
+    run()
